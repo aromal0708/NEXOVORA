18218bb50f61965c3c3fe8ee7cf9f66e23914cb7
Evan Punnen <evanpunnen123@gmail.com>
2025-07-21 12:08:37 +0530

    fix:some changes in build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar.tsx";
 import Footer from "./components/layout/Footer.tsx";
